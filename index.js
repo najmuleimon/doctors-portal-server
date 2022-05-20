ad4a5bb615cffe8a8b1bf33306904a6974363bbe
najmuleimon <najmul.eimon@gmail.com>
2022-05-20 23:51:48 +0600

    booking route added

--- index.js
+++ index.js
@@ -20,12 +20,52 @@ async function run() {
     try {
         await client.connect();
         const serviceCollection = client.db("doctorsPortal").collection("services");
+        const bookingCollection = client.db("doctorsPortal").collection("bookings");
 
+        // get all services
         app.get('/get-service', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services)
+        })
+
+        // post bookings
+        app.post('/booking', async (req, res) => {
+            const booking = req.body;
+            const query = { treatment: booking.treatment, date: booking.date, patient: booking.patient }
+            const exists = await bookingCollection.findOne(query);
+
+            if (exists) {
+                return res.send({ success: false, booking: exists })
+            }
+            const result = bookingCollection.insertOne(booking);
+            return res.send({ success: true, result });
+        })
+
+        // get my bookings
+        app.get('/booking', async (req, res) => {
+            const patient = req.query.patient;
+            const query = { patient: patient };
+            const bookings = await bookingCollection.find(query).toArray();
+            res.send(bookings);
+        })
+
+        // get available services
+        app.get('/available', async (req, res) => {
+            const date = req.query.date;
+            const services = await serviceCollection.find().toArray();
+
+            const query = { date: date }
+            const bookings = await bookingCollection.find(query).toArray();
+
+            services.forEach(service => {
+                const serviceBookings = bookings.filter(book => book.treatment === service.name);
+                const bookedSlots = serviceBookings.map(book => book.slot);
+                const available = service.slots.filter(slot => !bookedSlots.includes(slot));
+                service.slots = available;
+            })
+            res.send(services);
         })
     } finally {
 
